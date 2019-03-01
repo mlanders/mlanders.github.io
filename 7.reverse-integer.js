8756f18d33e9207f55b488f8a7f5892a6b9e4214
Michael Landers <landers.mike@gmail.com>
2019-02-28 20:20:18 -0800

    working on reverse-integer

---
+++ 7.reverse-integer.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=7 lang=javascript
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (25.12%)
+ * Total Accepted:    612.2K
+ * Total Submissions: 2.4M
+ * Testcase Example:  '123'
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 123
+ * Output: 321
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: -123
+ * Output: -321
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: 120
+ * Output: 21
+ *
+ *
+ * Note:
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of
+ * this problem, assume that your function returns 0 when the reversed integer
+ * overflows.
+ *
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+	let y = null;
+
+	y = String(Math.abs(x))
+		.split('')
+		.reverse()
+		.join('');
+
+	if (x < 0) {
+		return -Math.abs(Number(y));
+	} else {
+		return Number(y);
+	}
+};
+
+reverse(123);
+reverse(-123);
+reverse(1534236469);
